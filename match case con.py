023433a846aecc0f23ba780d6f0f7a45ea14995c
spandanagowda181-stack <spandanagowda181@gmail.com>
2025-12-07 13:14:12 +0530

    new file:   match case con.py

---
+++ match case con.py
@@ -0,0 +1,12 @@
+def number(x):
+    match x:
+        case 2:
+             print("two")
+        case 3:
+             print("three")
+        case  4:
+             print("four")
+        case _:
+            print("not found")
+number(3)
+number(5)
