e938daa596cdcd7681c8c8413755e5d39b82a0d0
spandanagowda181-stack <spandanagowda181@gmail.com>
2025-12-10 19:00:32 +0530

    first commit

---
+++ for2 loop.py
@@ -0,0 +1,12 @@
+
+for i in range(0,11,2):
+    print(i)
+name="spandana"
+for char in name:
+    print(char)
+    print(char*2)
+    print(char*3)
+sub_name="Gowda"
+for letter in sub_name:
+    print(letter*2)
+    
---
+++ nested for.py
@@ -0,0 +1,6 @@
+for i in range(2,11):
+    for j in range(1,11):
+        print(f"{i}*{j}=={i*j}")
+for i in range(1,10,2):
+    for j in range(1,11,1):
+        print(f"{i}*{j}=={i*j}")
